0d8852393455a4aab9b0ff5c9d349a8381beab24
Xianyx <pushkar44320@gmail.com>
2024-06-24 00:32:44 +0530

    more progress

---
+++ src/components/DisplayHome.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import NavBar from './NavBar'
+
+const DisplayHome = () => {
+  return (
+      <>
+          <NavBar/>
+      </>
+  )
+}
+
+export default DisplayHome
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -19,34 +19,28 @@ const Player = () => {
             src={assets.shuffle_icon}
             alt=""
           />
-          <img
-            className="w-4 cursor-pointer"
-            src={assets.prev_icon}
-            alt=""
-          />
-          <img
-            className="w-4 cursor-pointer"
-            src={assets.play_icon}
-            alt=""
-          />
-          <img
-            className="w-4 cursor-pointer"
-            src={assets.next_icon}
-            alt=""
-          />
-          <img
-            className="w-4 cursor-pointer"
-            src={assets.loop_icon}
-            alt=""
-          />
+          <img className="w-4 cursor-pointer" src={assets.prev_icon} alt="" />
+          <img className="w-4 cursor-pointer" src={assets.play_icon} alt="" />
+          <img className="w-4 cursor-pointer" src={assets.next_icon} alt="" />
+          <img className="w-4 cursor-pointer" src={assets.loop_icon} alt="" />
         </div>
-        <div className='flex items-center gap-5'>
+        <div className="flex items-center gap-5">
           <p>1:03</p>
-          <div className='w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer'>
-            <hr className='h-1 border-none w-0 bg-green-800 rounded-full' />
+          <div className="w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer">
+            <hr className="h-1 border-none w-0 bg-green-800 rounded-full" />
           </div>
           <p>3:45</p>
         </div>
+      </div>
+      <div className="hidden lg:flex items-center gap-2 opacity-75">
+        <img className="w-4" src={assets.plays_icon} alt="" />
+        <img className="w-4" src={assets.mic_icon} alt="" />
+        <img className="w-4" src={assets.queue_icon} alt="" />
+        <img className="w-4" src={assets.speaker_icon} alt="" />
+        <img className="w-4" src={assets.volume_icon} alt="" />
+        <div className='w-20 bg-slate-50 h-1 rounded'></div>
+        <img className="w-4" src={assets.mini_player_icon} alt="" />
+        <img className="w-4" src={assets.zoom_icon} alt="" />
       </div>
     </div>
   );
