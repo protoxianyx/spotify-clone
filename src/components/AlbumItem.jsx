bd5391e216b40e45fb770e49eaefbb14f3c0548f
Xianyx <pushkar44320@gmail.com>
2024-07-16 14:35:57 +0530

    dead end

---
+++ src/components/AlbumItem.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const AlbumItem = (image, name, desc, id) => {
+  return (
+    <div className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
+      <img className="rounded" src={image} alt="" />
+    </div>
+  );
+};
+
+export default AlbumItem;
--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,13 +1,26 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import NavBar from './NavBar'
+import React from "react";
+import NavBar from "./NavBar";
+import { albumsData } from "../assets/assets";
+import AlbumItem from "./AlbumItem";
 
 const DisplayHome = () => {
   return (
-      <>
-          <NavBar/>
-      </>
-  )
-}
+    <>
+      <NavBar />
+      <div className="my-5 font-bold text-2xl">
+        {albumsData.map((item, index) => (
+          <AlbumItem
+            key={index}
+            name={item.name}
+            desc={item.desc}
+            id={item.id}
+            image={item.image}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default DisplayHome
+export default DisplayHome;
